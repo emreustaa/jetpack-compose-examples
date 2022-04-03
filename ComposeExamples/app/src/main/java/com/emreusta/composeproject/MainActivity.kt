cb2f09f162dd0bb7ae1a6f6867987e08910f4ed5
Emre USTA <emre.usta@stu.fsm.edu.tr>
2022-04-03 19:23:54 +0300

    project created

---
+++ ComposeExamples/app/src/main/java/com/emreusta/composeproject/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.emreusta.composeproject
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.emreusta.composeproject.ui.theme.ComposeProjectTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeProjectTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Sayfa()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Sayfa() {
+    Text(text = "Merhaba Dünya!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ComposeProjectTheme {
+        Sayfa( )
+    }
+}
