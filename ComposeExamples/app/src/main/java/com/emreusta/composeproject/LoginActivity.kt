9bc7883a29a34f7e8d904b9f085bba514ef6def1
Emre USTA <emre.usta@stu.fsm.edu.tr>
2022-04-04 03:01:44 +0300

    example login app completed

---
+++ ComposeExamples/app/src/main/java/com/emreusta/composeproject/LoginActivity.kt
@@ -0,0 +1,100 @@
+package com.emreusta.composeproject
+
+import android.os.Bundle
+import android.text.Editable
+import android.util.Log
+import android.widget.EditText
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextLayoutInput
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.emreusta.composeproject.ui.theme.ComposeProjectTheme
+
+class LoginActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeProjectTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    MainPage()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MainPage() {
+    Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.primary) {
+        val tfKullaniciAdi = remember {
+            mutableStateOf("")
+        }
+        val tfSifre = remember {
+            mutableStateOf("")
+        }
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceEvenly,
+        ) {
+            Image(painter = painterResource(id = R.drawable.ic_login), contentDescription = "")
+            TextField(
+                value = tfKullaniciAdi.value,
+                onValueChange = { tfKullaniciAdi.value = it },
+                label = { Text(text = "Kullanıcı Adı") },
+                colors = TextFieldDefaults.textFieldColors(
+                    backgroundColor = Color.White,
+                    textColor = Color.Black,
+                    focusedIndicatorColor = Color.Red
+                )
+            )
+            TextField(
+                value = tfSifre.value,
+                onValueChange = { tfSifre.value = it },
+                label = { Text(text = "Şifre") },
+                colors = TextFieldDefaults.textFieldColors(
+                    backgroundColor = Color.White,
+                    textColor = Color.Black,
+                    focusedIndicatorColor = Color.Red
+                )
+            )
+
+            Button(
+                onClick = { Log.e("Button", "Giriş Yapıldı") },
+                colors = ButtonDefaults.buttonColors(
+                    backgroundColor = Color.Red,
+                    contentColor = Color.White
+                ),
+                modifier = Modifier.size(250.dp, 50.dp)
+            ) {
+                Text(text = "Giriş Tap")
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview3() {
+    ComposeProjectTheme {
+        MainPage()
+    }
+}
